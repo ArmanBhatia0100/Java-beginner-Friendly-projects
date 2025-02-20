f45526935789db6b414ef8c5c8f5ec2927c521f1
Arman Bhatia <bhat0100@algonquinlive.com>
2025-02-20 22:06:03 +0000

    Library_management, Added feature to read from file.

--- Library_Management_System/src/main/java/arman/library_management_system/Library_Management_System.java
+++ Library_Management_System/src/main/java/arman/library_management_system/Library_Management_System.java
@@ -3,6 +3,8 @@
  */
 package arman.library_management_system;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,6 +21,7 @@ public class Library_Management_System {
     private static Library library = new Library();
 
     public static void main(String[] args) {
+        loadAllBooksFromFile();
 
         try {
             int userMenuSelection;
@@ -31,7 +34,7 @@ public class Library_Management_System {
                         String bookTitle = sc.next().trim().toLowerCase();
                         System.out.println("Who is the author of the book?");
                         String bookAuthor = sc.next().trim().toLowerCase();
-                        addBook(new Book(bookTitle, bookAuthor));
+                        addBook(new Book(bookTitle, bookAuthor), true);
                     }
                     case 2 -> {
                         System.out.println("Provide the keyword for the book title.");
@@ -78,8 +81,13 @@ public class Library_Management_System {
         return sc.nextInt();
     }
 
-    private static void addBook(Book book) {
-        System.out.println(library.addBook(book));
+    private static void addBook(Book book, Boolean showSuccessNotification) {
+
+        if (!showSuccessNotification) {
+            library.addBook(book);
+        } else {
+            System.out.println(library.addBook(book));
+        }
 
     }
 
@@ -117,8 +125,8 @@ public class Library_Management_System {
     private static void addCheckedoutBookToFile(Book book) throws IOException {
         String filename = "issuedBook.txt";
         FileWriter fs = new FileWriter(filename, true);
-        fs.write(book.getTitle() + " " + book.getAuthor()+"\n");
-        
+        fs.write(book.getTitle() + " " + book.getAuthor() + "\n");
+
         System.out.println("File Successfully Created.");
         fs.close();
     }
@@ -144,6 +152,28 @@ public class Library_Management_System {
         for (int i = 0; i < library.getAvailableBooks().size(); i++) {
             System.out.println(i + ". " + library.getAvailableBooks().get(i));
         }
+    }
 
+    private static void loadAllBooksFromFile() {
+
+        try {
+
+            FileReader fr = new FileReader("allAvailableBook.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+
+            //If the stream is read and the file is not null.
+            if (br.ready()) {
+                while ((line = br.readLine()) != null) {
+                    String[] titleAndAuth = line.split(" ");
+
+                    Book newBook = new Book(titleAndAuth[0], titleAndAuth[1]);
+                    addBook(newBook, false);
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("Books from allAvailableBooks cannot be loaded");
+        }
     }
 }
